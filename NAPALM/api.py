6b7a7d450812a5424b361630548498fbca947c10
Finbar Pimlott <Pimlotb@users.noreply.github.com>
2025-02-20 10:54:45 +0000

    Update api.py

--- NAPALM/api.py
+++ NAPALM/api.py
@@ -1,5 +1,5 @@
 import requests
-headers={"Authorization": "Bearer 891f4807-05a1-4f0a-b27d-d91a2e5df844"}
+headers={"Authorization": "Bearer *************"}
 response= requests.get("https://vco22-fra1.velocloud.net//api/sdwan/v2", headers=header)
 print(response.json)
 
